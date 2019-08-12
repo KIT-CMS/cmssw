62c7022c31a03b67a569f44aa91610fc8c8bd7d2
Sebastian Brommer <sebastian.brommer@student.kit.edu>
2019-08-12 13:12:39 +0200

    fix indent

--- TauAnalysis/MCEmbeddingTools/python/customisers.py
+++ TauAnalysis/MCEmbeddingTools/python/customisers.py
@@ -441,8 +441,8 @@ def customiseMerging(process, changeProcessname=True,reselect=False):
 	process.merge_step += process.genForPF2PATSequence
 
 	process.schedule.insert(0,process.merge_step)
-    process.slimmedPatTrigger.triggerResults =  cms.InputTag("TriggerResults","","SIMembedding")
-	 # process.load('PhysicsTools.PatAlgos.slimming.slimmedGenJets_cfi')
+	# process.slimmedPatTrigger.triggerResults =  cms.InputTag("TriggerResults","","SIMembedding")
+	# process.load('PhysicsTools.PatAlgos.slimming.slimmedGenJets_cfi')
 
 	process = customisoptions(process)
 	return modify_outputModules(process, [keepMerged(dataTier)])
